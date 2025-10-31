014aec4ea6feec32b56d9268e50b21f96494d273
jackrapanlt <jackrapan.lt@gmail.com>
2025-10-31 09:29:42 +0700

    TranTPBoss

---
+++ Assets/Scripts/LoadSceneBoss.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneBoss : MonoBehaviour
+{
+    [SerializeField] private string bossSceneName = "Boss";
+
+    void Start()
+    {
+        SceneManager.LoadScene(bossSceneName);
+    }
+}
